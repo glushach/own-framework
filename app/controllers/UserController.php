005cc64258e48803be8bdea973d3ff6a7793558c
Oleksandr Hlushach <oleksandr.hlushach@ristars.io>
2023-11-04 22:14:06 +0200

    add validate is register user

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -13,11 +13,18 @@ class UserController extends AppController
       $user = new User();
       $data = $_POST;
       $user->load($data);
-      if(!$user->validate($data)) {
+      if(!$user->validate($data) || !$user->checkUnique()) {
         $user->getErrors();
+        $_SESSION['form_data'] = $data;
         redirect();
       }
-      die;
+      $user->attributes['password'] = password_hash($user->attributes['password'], PASSWORD_DEFAULT);
+      if ($user->save('user')) {
+        $_SESSION['success'] = 'Вы успешно зарегистрированы';
+      } else {
+        $_SESSION['error'] = 'Ошибка! Попробуйте позже';
+      }
+      redirect();
     }
     View::setMeta('Регистрация');
   }
--- app/views/User/signup.php
+++ app/views/User/signup.php
@@ -4,7 +4,8 @@
     <form method="post" action="/user/signup">
       <div class="form-group mb-3">
         <label for="login">Login</label>
-        <input type="text" name="login" class="form-control" id="login" placeholder="Login">
+        <input type="text" name="login" class="form-control" id="login" placeholder="Login" 
+        value="<?= isset($_SESSION['form_data']['login']) ? h($_SESSION['form_data']['login']) : ''; ?>">
       </div>
       <div class="form-group mb-3">
         <label for="password">Password</label>
@@ -12,13 +13,16 @@
       </div>
       <div class="form-group mb-3">
         <label for="name">Имя</label>
-        <input type="text" name="name" class="form-control" id="name" placeholder="Имя">
+        <input type="text" name="name" class="form-control" id="name" placeholder="Имя"
+        value="<?= isset($_SESSION['form_data']['name']) ? h($_SESSION['form_data']['name']) : ''; ?>">
       </div>
       <div class="form-group mb-3">
         <label for="email">Email</label>
-        <input type="text" name="email" class="form-control" id="email" placeholder="Email">
+        <input type="text" name="email" class="form-control" id="email" placeholder="Email" 
+        value="<?= isset($_SESSION['form_data']['email']) ? h($_SESSION['form_data']['email']) : ''; ?>">
       </div>
       <button type="submit" class="btn btn-primary">Зарегистрировать</button>
     </form>
+    <?php if(isset($_SESSION['form_data'])) unset($_SESSION['form_data']); ?>
   </div>
 </div>
