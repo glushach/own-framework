63ae146fb95f6323f65b3da4c508296c55e26cd1
Oleksandr Hlushach <oleksandr.hlushach@ristars.io>
2023-11-04 20:43:12 +0200

    add validation error

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -13,10 +13,9 @@ class UserController extends AppController
       $user = new User();
       $data = $_POST;
       $user->load($data);
-      if($user->validate($data)) {
-        echo 'OK';
-      } else {
-        echo 'No';
+      if(!$user->validate($data)) {
+        $user->getErrors();
+        redirect();
       }
       die;
     }
--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -26,6 +26,18 @@
       </div>
     </nav>
 
+    <?php if(isset($_SESSION['error'])) : ?>
+      <div class="alert alert-danger">
+        <?= $_SESSION['error']; unset($_SESSION['error']) ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if(isset($_SESSION['success'])) : ?>
+      <div class="alert alert-success">
+        <?= $_SESSION['success']; unset($_SESSION['success']) ?>
+      </div>
+    <?php endif; ?>
+
     <?=$content;?>
 
     <!-- <?= debug(\fw\core\Db::$countSql) ?> -->
