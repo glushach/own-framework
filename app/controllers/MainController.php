d49e9f0bb595ffb8f3c6448256fc2acd27e00349
Oleksandr Hlushach <oleksandr.hlushach@ristars.io>
2023-11-05 19:05:43 +0200

    add pagination

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 use app\models\Main;
 use fw\core\App;
 use fw\core\base\View;
+use fw\libs\Pagination;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use PHPMailer\PHPMailer\PHPMailer;
@@ -31,18 +32,24 @@ class MainController extends AppController
     // echo $test;
     // trigger_error("E_USER_ERROR", E_USER_ERROR);
 
-    $posts = App::$app->cache->get('posts');
-    if (!$posts) {
-      $posts = \R::findAll('posts');
-      App::$app->cache->set('posts', $posts, 3600 * 24);
-    }
-    $post = \R::findOne('posts', 'id = 1');
+    $total = \R::count('posts');
+    $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+    $perpage = 1;
+
+    $pagination = new Pagination($page, $perpage, $total);
+    $start = $pagination->getStart();
+
+    // $posts = App::$app->cache->get('posts');
+    // if (!$posts) {
+      $posts = \R::findAll('posts', "LIMIT $start, $perpage");
+      // App::$app->cache->set('posts', $posts, 3600 * 24);
+    // }
     $menu = $this->menu;
     $title = 'PAGE TITLE';
     // $this->setMeta('Главная страница', 'Описание страница', 'Ключевые слова');
     // $meta = $this->meta;
     View::setMeta('Главная страница', 'Описание страница', 'Ключевые слова');
-    $this->set(compact('title', 'posts', 'menu'));
+    $this->set(compact('title', 'posts', 'menu', 'pagination', 'total'));
   }
 
   public function testAction()
